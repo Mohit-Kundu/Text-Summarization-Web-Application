1be44e923a8a0e246d98d45043241962112f6b3f
Mohit-Kundu <moh8.kundu@gmail.com>
2021-07-25 18:15:02 +0530

    Found word frequencies

--- summarizer.py
+++ summarizer.py
@@ -5,3 +5,28 @@ from scraper import scraper
 
 stopwords = list(STOP_WORDS)
 print(stopwords)
+
+nlp = spacy.load('en_core_web_sm')
+
+# Scraping text from article
+text = scraper('https://en.wikipedia.org/wiki/Natural_language_processing')
+
+# Saving tokenized sentence
+doc = nlp(text)
+
+tokens = [token.text for token in doc]
+#print(tokens)
+
+# Finding Word Frequencies
+word_frequencies = {}
+
+for word in doc:
+    if word.text.lower() not in stopwords:
+        if word.text.lower() not in punctuation:
+            if word.text.lower() not in word_frequencies.keys():
+                #Adding new word to word_freequeny
+                word_frequencies[word.text.lower()] = 1
+            else:
+                word_frequencies[word.text.lower()] += 1
+
+print(word_frequencies)
