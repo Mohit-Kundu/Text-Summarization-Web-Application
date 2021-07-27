d179d4e78c3c0199d4a2dba3940b806b365450a2
Mohit-Kundu <moh8.kundu@gmail.com>
2021-07-28 00:46:52 +0530

    Added functionality to scrape div tags

--- scraper.py
+++ scraper.py
@@ -16,15 +16,25 @@ def scraper(url):
 
     title = parsed_article.find('h1').text
 
-    paragraphs = parsed_article.find_all('p')
-
-    for p in paragraphs:
-        article_text += p.text
-
     # Removing Square Brackets and Extra Spaces
     def remove_square_brackets(article_text):
         article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
         return re.sub(r'\s+', ' ', article_text)
 
+    paragraphs = parsed_article.find_all('p')
+
+    # Checking content of p tags
+    if len(paragraphs) != 0:
+        for p in paragraphs:
+            article_text += p.text
+    
+    # Checking content of div tags
+    else:
+        divs = parsed_article.find_all('div', id = 'container')
+
+        for div in divs:
+            article_text += div.text
+
     body = remove_square_brackets(article_text)
+        
     return title, body
