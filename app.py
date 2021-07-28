e80c926b781243ed7e0f5f548de31348873fe611
Mohit-Kundu <moh8.kundu@gmail.com>
2021-07-28 23:25:39 +0530

    Updated app.py to display summary in textarea tag

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ def index():
         url = request.form.get('url')
         title, text = scraper(url)
         summary = summarizer(text)
+        summary_html = '<textarea>' + summary + '</textarea>'
         return summary
-        
-    return render_template("index.html")
+
+    else: 
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug = True) 
