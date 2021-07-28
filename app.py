3cde06f7071a232cbcb89dee00b72a506e85d2bb
Mohit-Kundu <moh8.kundu@gmail.com>
2021-07-28 19:49:55 +0530

    Linked app.py with scrape.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, request, url_for
+from scraper import scraper
 
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return "summary"
+        url = request.form.get('url')
+        #return url
+        title, text = scraper(url)
+        return title,text
+        
 
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True) 
