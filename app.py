6364c9e9258b47e75c172c02a2cc74c2d2c5f8ad
Mohit-Kundu <moh8.kundu@gmail.com>
2021-07-28 11:34:33 +0530

    Added GET and POST methods

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        return "summary"
+
     return render_template("index.html")
 
 if __name__ == "__main__":
